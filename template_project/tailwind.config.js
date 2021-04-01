c680d222353b7a68f740bb0dc7819add534f8181
Aravinda Vishwanathapura <aravinda@kadalu.io>
2021-04-01 11:36:51 +0530

    Added tailwind css
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.io>

---
+++ template_project/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: [
+        './layouts/**/*.html',
+        './content/**/*.html',
+        './content/**/*.md',
+        './content/**/*.adoc'
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
